6618303bfb3e8f77220c6e1e1d2f2d84514663b5
Michał Górski <michal.gorski.pl@gmail.com>
2021-08-30 16:47:38 +0200

    Remove obsolete ApiGateway and update python runtime

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
-# AWS Orphaned Log Groups Collector
-Orphaned Log Groups Collector deletes log groups not associated with any resources in the AWS Account.
+# AWS Lambda Orphaned Log Groups Collector
+Orphaned Log Groups Collector deletes AWS Lambda related log groups not associated with any AWS Lambda in the AWS Account.
+
+Available on the [AWS Serverless Application Repository](https://aws.amazon.com/serverless)
 
 ## Why is it helpful?
 Deleting AWS resources even using CloudFormation does not delete the corresponding CloudWatch Log Groups what cause unnecessary costs.
 
 ### Supported services
 - AWS Lambda
-- Amazon API Gateway
 
 ## Getting Started
 ### Prerequisites
-- Python 3.7+
+- Python 3.8+
 - [AWS SAM CLI](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install.html)
 
 ### Deployment
@@ -19,20 +20,20 @@ To deploy Orphaned Log Group Collector edit deploy.bat file and change
 - \<s3-bucket-name> - s3 bucket name for code and template file needed for deployment of the AWS Lambda
 - \<s3-bucket-prefix> - logical folder for the code and template file
 
-Then run the file.
+Then run the Lambda.
 
 ### Input Lambda Event
 ```json
 {
-    "view_only": true,
+    "dry_run": true,
     "regions": ["us-west-1", "..."]
 }
 ```
 ### Default values
-- view_only: true
+- dry_run: true
 - regions: all regions
 
-# **It is strongly recommended to use view_only mode first!!**
+# **It is strongly recommended to use dry_run mode first!!**
 
 ## Scheduler
 Orphaned Log Groups Collector by default is scheduled every first day of the month. 
--- code/handler.py
+++ code/handler.py
@@ -1,23 +1,21 @@
 import json
 import boto3
 from engines.lambda_engine import LambdaEngine
-from engines.apigateway_engine import ApigatewayEngine
 
 
 def lambda_handler(event, context):
-    view_only = get_view_only_parameter(event)
+    dry_run = get_dry_run_parameter(event)
     regions = get_regions_parameter(event)
 
     for region in regions:
         print('REGION: {}'.format(region))
         logs_client = boto3.client('logs', region_name=region)
         orphaned_log_groups = find_orphans(region, logs_client)
-        delete_log_groups(orphaned_log_groups, logs_client, region, view_only)
+        delete_log_groups(orphaned_log_groups, logs_client, region, dry_run)
 
 
 def find_orphans(region, logs_client):
     lambda_engine = LambdaEngine(region)
-    apigateway_engine = ApigatewayEngine(region)
 
     orphaned_log_groups = []
 
@@ -25,27 +23,26 @@ def find_orphans(region, logs_client):
     for response in paginator.paginate():
         for log_group in response.get('logGroups'):
             log_group_name = log_group['logGroupName']
-            if lambda_engine.is_orphan(log_group_name) or apigateway_engine.is_orphan(log_group_name):
+            if lambda_engine.is_orphan(log_group_name):
                 orphaned_log_groups.append(log_group)
 
     return orphaned_log_groups
 
 
-def delete_log_groups(orphaned_log_groups, logs_client, region, view_only):
-    if view_only:
-        print(json.dumps(orphaned_log_groups))
-    else:
+def delete_log_groups(orphaned_log_groups, logs_client, region, dry_run):
+    print(json.dumps(orphaned_log_groups))
+    if not dry_run:
         for log_group in orphaned_log_groups:
             logs_client.delete_log_group(logGroupName=log_group['logGroupName'])
 
     print('There were {} orphaned log groups in {}.\n'.format(len(orphaned_log_groups), region))
 
 
-def get_view_only_parameter(event):
-    if 'view_only' not in event:
+def get_dry_run_parameter(event):
+    if 'dry_run' not in event:
         return 1
     else:
-        return event['view_only']
+        return event['dry_run']
 
 
 def get_regions_parameter(event):
